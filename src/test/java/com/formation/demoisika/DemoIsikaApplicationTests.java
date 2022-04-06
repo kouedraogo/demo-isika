7f2011ca945341ad7a40e35a22ff1547a4cf5ca8
vagrant <vagrant@jenkins>
2022-04-06 14:01:01 +0000

    Adding test

--- src/test/java/com/formation/demoisika/DemoIsikaApplicationTests.java
+++ src/test/java/com/formation/demoisika/DemoIsikaApplicationTests.java
@@ -1,13 +1,22 @@
 package com.formation.demoisika;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.formation.demoisika.service.CalculatorService;
 
 @SpringBootTest
 class DemoIsikaApplicationTests {
+	
+	@Autowired
+	private CalculatorService service;
 
 	@Test
-	void contextLoads() {
+	void addTest() {
+		assertEquals(12, service.add(8, 4));
 	}
 
 }
